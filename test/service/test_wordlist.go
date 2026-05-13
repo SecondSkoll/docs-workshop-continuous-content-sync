cfa74febebb03c66a9294a881f79f96be1316bbd
Michael Park <michael.park@canonical.com>
2026-05-14 00:36:49 +1000

    reset main

--- test/service/test_wordlist.go
+++
@@ -1,9 +0,0 @@
-//go:build test
-
-package service
-
-// Testing wordlist that will always print `a a a a`.
-var wordlist = `1	a
-2	a
-3	a
-4	a`
--- test/test/lint/capitalize-errors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -eu
-
-echo "Checking for error messages beginning with lower-case letters..."
-
-! git grep --untracked -P -n 'fmt\.Errorf\("[a-z]' -- '*.go'
